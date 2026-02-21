43e55872f8ed7181026c2d41a34df5e5f6c0256a
umitonel <umitonel@gmail.com>
2026-02-21 10:52:32 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask
+import os
 
 app = Flask(__name__)
 
@@ -7,4 +8,5 @@ def home():
     return "Crypto Sinyal Paneli Aktif 🚀"
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=3000)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
